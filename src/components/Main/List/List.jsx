c4006c492130d4089a2c86e551d7c97e63382eae
Debid Magar <linkwithdm@gmail.com>
2021-01-02 12:27:38 +0545

    List added

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,32 +1,86 @@
-import React from 'react'
-import {List as MUList , ListItem, ListItemAvatar, ListItemText ,Avatar , ListItemSecondaryAction,IconButton,Slide} from "@material-ui/core"
-import {Delete, MoneyOff} from "@material-ui/icons"
+import React from "react";
+import {
+  List as MUList,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Avatar,
+  ListItemSecondaryAction,
+  IconButton,
+  Slide,
+} from "@material-ui/core";
+import { Delete, MoneyOff } from "@material-ui/icons";
 
-import useStyles from './styles'
+import useStyles from "./styles";
 
 function List() {
-    const classes = useStyles()
-    const transctions =[{
-        id:1
-    }]
-    return (
-        <MUList dense={false} className={classes.list}>
-            {
-                transctions.map((transction)=>(
-                    <Slide  direction="down" in mountOnEnter unmountOnExit key={transction.id}>
-
-<ListItem>
-    <ListItemAvatar>
-        <Avatar className={transction.type==='Income'?classes.avatarIncome : classes.avatarExpense}>
-
-        </Avatar>
-    </ListItemAvatar>
-</ListItem>
-                    </Slide>
-                ))
-            }
-        </MUList>
-    )
+  const classes = useStyles();
+  const transctions = [
+    {
+      id: 1,
+      type: "Income",
+      category: "Salary",
+      amount: 30,
+      date: "Sat Jan 02 2021",
+    },
+    {
+      id: 2,
+      type: "Expense",
+      category: "Salary",
+      amount: 30,
+      date: "Sat Jan 02 2021",
+    },
+    {
+      id: 3,
+      type: "Income",
+      category: "Salary",
+      amount: 30,
+      date: "Sat Jan 02 2021",
+    },
+    {
+      id: 4,
+      type: "Income",
+      category: "Salary",
+      amount: 30,
+      date: "Sat Jan 02 2021",
+    },
+  ];
+  return (
+    <MUList dense={false} className={classes.list}>
+      {transctions.map((transction) => (
+        <Slide
+          direction="down"
+          in
+          mountOnEnter
+          unmountOnExit
+          key={transction.id}
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar
+                className={
+                  transction.type === "Income"
+                    ? classes.avatarIncome
+                    : classes.avatarExpense
+                }
+              >
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={transction.category}
+              secondary={`$${transction.amount} - ${transction.date}`}
+            />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete" onClick="">
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
+    </MUList>
+  );
 }
 
-export default List
+export default List;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,29 +1,42 @@
-import React from 'react'
-import {Card,CardHeader,CardContent,Typography,Grid,Divider} from "@material-ui/core"
-import useStyles from "./styles.js"
-import Form from './Form/Form.jsx'
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+import useStyles from "./styles.js";
+import Form from "./Form/Form.jsx";
+import List from "./List/List.jsx";
 function Main() {
-    const classes = useStyles()
-    return (
-        <Card className={classes.root}>
-            <CardHeader title="Expesne Tracker" subheader="dfvd"/>
-            <CardContent>
-                <Typography align ="center" variant="h5">Total Balance  </Typography>
-                <Typography variant="subtitle1" style={{lineheight:'1.5em',marginTop:'20px'}}>Total Balance  </Typography>
-                <Divider />
-                <Form/>
-            </CardContent>
-            <CardContent className={classes.cartContent}>
-                <Grid container spacing={2}>
-                <Grid item xs={12}>
-                   
-                   </Grid>
-
-                </Grid>
-               
-            </CardContent>
-        </Card>
-    )
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expesne Tracker" subheader="dfvd" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance{" "}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineheight: "1.5em", marginTop: "20px" }}
+        >
+          Total Balance{" "}
+        </Typography>
+        <Divider />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cartContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default Main
+export default Main;
